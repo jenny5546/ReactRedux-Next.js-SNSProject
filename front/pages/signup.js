8c64f870be99441f49156b2d4829b6c52ab6e6bd
jenny5546 <jenny5546@naver.com>
2020-02-02 16:41:33 +0900

    added profile.js signup.js

---
+++ front/pages/signup.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Head from 'next/head';
+import AppLayout from '../components/AppLayout';
+
+const Signup = () => {
+    return (
+        <>
+            <Head>
+                <title>NodeBird</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css" />
+            </Head>
+            <AppLayout>
+                <div>
+                    Signup
+                </div>
+            </AppLayout>
+        </>
+    );
+};
+
+export default Signup;
